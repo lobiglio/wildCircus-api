6654cdf912fc0e68f87ddc51d16c0be84202d845
MrWhite <hugoblanc.blend@gmail.com>
2019-12-12 18:24:29 +0100

    fix: add mysql to load + npm i mysql

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,9 +1,13 @@
 import expressLoader from './express';
 import { Application } from 'express';
+import mysqlLoader from './mysql';
 
 export default async (app: Application ) => {
+  console.log('Express start init');
   await expressLoader(app);
-  console.log('Express Intialized');
+  console.log('Mysql Intialized correctly');
 
-  // Rajouter le loader MySQL
+  console.log('Mysql Start init');
+  await mysqlLoader();
+  console.log('Mysql Intialized correctly');
 };
