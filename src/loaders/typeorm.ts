724d472ee79ba3c659d2acef0572e1c3164171bd
letizia obiglio <letizia.obiglio@gmail.com>
2020-01-29 15:06:22 +0100

    build: loader typeorm filled, express running

--- src/loaders/typeorm.ts
+++ src/loaders/typeorm.ts
@@ -3,16 +3,16 @@ import { User } from '../entity/user.entity';
 
 export default async () => {
 
-await createConnection({
-    type: 'mysql',
-    host: process.env.TEMPLATE_DB_HOST,
-    port: parseInt(process.env.TEMPLATE_DB_PORT || '3306', 10),
-    username: process.env.TEMPLATE_DB_USER,
-    password: process.env.TEMPLATE_DB_PASS, 
-    database: process.env.TEMPLATE_DB,
-    entities: [
-        User,
-    ],
-    synchronize: true,
-});
+    await createConnection({
+        type: 'mysql',
+        host: 'localhost',
+        port: parseInt('3306', 10),
+        username: process.env.USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.WILDCIRCUS_DB,
+        entities: [
+            User,
+        ],
+        synchronize: true,
+    });
 };
